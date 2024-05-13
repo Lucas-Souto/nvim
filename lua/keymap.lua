6f98b11aafc0aa05fe0259647929326cb429a592
= <contato.lucassouto@outlook.com>
2024-05-13 15:54:18 -0300

    Customização para abas.

--- lua/keymap.lua
+++ lua/keymap.lua
@@ -25,14 +25,15 @@ keymap('v', "'", ":lua require('functions').embrace_selection(\"'\", \"'\")<cr>"
 keymap('i', '<CR>', 'getline(".")[col(".") - 2] == "{" && getline(".")[col(".") - 1] == "}" ? "<Left><CR><Right><CR><CR><Up><Tab>" : (getline(".")[col(".") - 2] == "[" && getline(".")[col(".") - 1] == "]" ? "<Left><CR><Right><CR><CR><Up><Tab>" : "<CR>")', ie_opt)
 
 -- init.lua
-keymap('n', '<C-r>r', ':source $MYVIMRC<cr>', n_opt)
+keymap('n', '<C-r>r', ':source $MYVIMRC<cr>:echo "Pronto!"<cr>', n_opt)
 keymap('n', '<C-r>o', ':e $MYVIMRC<cr>', n_opt)
 
 -- CTRL + S para salvar
 keymap('n', '<C-s>', ':w<cr>', n_opt)
 
--- Atalho para abrir o Fern
+-- Atalhos para abrir o Fern
 keymap('n', '<C-f>o', ':Fern . -drawer<cr>', n_opt)
+keymap('n', '<C-f>p', ':Fern .<cr>', n_opt)
 
 -- Navegação entre janelas
 keymap('n', '<C-Right>', '<C-w>l', n_opt)
@@ -45,3 +46,10 @@ keymap('n', '<C-w><Left>', ':vertical resize -2<cr>', n_opt)
 keymap('n', '<C-w><Right>', ':vertical resize +2<cr>', n_opt)
 keymap('n', '<C-w><Up>', ':resize -2<cr>', n_opt)
 keymap('n', '<C-w><Down>', ':resize +2<cr>', n_opt)
+
+-- Manipulação de abas
+keymap('n', '<C-t>n', ':tabnew<cr>', n_opt)
+keymap('n', '<C-t>w', ':tabclose<cr>', n_opt)
+keymap('n', '<C-t>W', ':tabonly<cr>', n_opt)
+keymap('n', '<C-t><Left>', ':-tabnext<cr>', n_opt)
+keymap('n', '<C-t><Right>', ':+tabnext<cr>', n_opt)
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,12 +9,14 @@ Plug('lambdalisue/vim-fern-renderer-nerdfont')-- Renderer para ícones
 Plug('lambdalisue/vim-glyph-palette')-- Cores para os ícones
 Plug('numToStr/Comment.nvim')-- Comentar linhas e seleção
 Plug('mattn/emmet-vim')-- Emmet
-Plug('nvim-lualine/lualine.nvim')-- Footer
+Plug('nvim-lualine/lualine.nvim')-- Status
 Plug('norcalli/nvim-colorizer.lua')-- Preview de cores
 Plug('tpope/vim-fugitive')-- Comandos git
 Plug('anuvyklack/pretty-fold.nvim')-- Customização de fold
 Plug('anuvyklack/keymap-amend.nvim')-- Dependência do fold-preview
 Plug('anuvyklack/fold-preview.nvim')-- Preview de fold
+Plug('lukas-reineke/indent-blankline.nvim')-- Linhas guia de escopo
+Plug('nanozuki/tabby.nvim')-- Abas
 -- Code complete
 Plug('Shougo/deoplete.nvim') 
 Plug('zchee/deoplete-clang')
@@ -37,9 +39,17 @@ require('colorizer').setup { css = { rgb_fn = true, hsl_fn = true }, html = { mo
 vim.g['deoplete#sources#clang#libclang_path'] = '/usr/lib/llvm-18/lib/libclang-18.so.18'
 vim.g['deoplete#sources#clang#clang_header'] = '/usr/lib/llvm-18/lib/clang/18/include/'
 vim.g['deoplete#enable_at_startup'] = 1
-
 vim.cmd('call deoplete#custom#option("num_processes", 4)')
 vim.g["deoplete#source#ternjs#docs"] = 1
 
 require('pretty-fold').setup()
 require('fold-preview').setup { auto = 400 }
+
+require('ibl').setup()
+
+require('tabby.tabline').use_preset('tab_only',
+{
+	nerdfont = true,
+	lualine_theme = 'auto',
+	buf_name = { mode = "'unique'|'relative'|'tail'|'shorten'" }
+})
