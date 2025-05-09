7af61e2134e7d7de200be43ec87e6927fc65ea50
Lucas Souto <contato.lucassouto@outlook.com>
2025-05-08 21:42:19 -0300

    .

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ vim.opt.softtabstop = 4
 vim.opt.smartindent = true
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus'-- xclip
 vim.opt.wrap = false
 vim.opt.swapfile = false
 vim.opt.completeopt:remove('preview')
